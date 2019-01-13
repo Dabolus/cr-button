342e6a4d2d2460f0bedadea40557d9a0bc98e12f
Giorgio Garasto <giorgio@garasto.it>
2019-01-13 21:30:51 +0100

    chore: Add Rollup config

---
+++ rollup.config.ts
@@ -0,0 +1,61 @@
+// tslint:disable:object-literal-sort-keys
+import { readdirSync } from 'fs';
+import { resolve } from 'path';
+import minifyHtml from 'rollup-plugin-minify-html-literals';
+import { terser } from 'rollup-plugin-terser';
+import typescript from 'rollup-plugin-typescript2';
+
+const prod = process.env.NODE_ENV === 'production';
+
+const inputs = readdirSync('src')
+  .filter((file) => !file.endsWith('.d.ts'))
+  .map((file) => file.slice(0, -3));
+
+const getConfig = (input, minify) => ({
+  input: `src/${input}.ts`,
+  output: [{
+    file: `${input}${minify ? '.min' : ''}.js`,
+    format: 'es',
+    sourcemap: !prod,
+  }, {
+    file: `${input}.iife${minify ? '.min' : ''}.js`,
+    format: 'iife',
+    name: `dabolus.${input.replace(/-([a-z])/g, ([, l]) => l.toUpperCase())}`,
+    sourcemap: !prod,
+  }],
+  plugins: [
+    typescript({
+      tsconfigOverride: {
+        compilerOptions: {
+          declaration: prod,
+          sourceMap: !prod,
+        },
+      },
+    }),
+    ...minify ? [
+      minifyHtml({
+        options: {
+          shouldMinify: (template) => /\s*<[a-z](?:[^`\\]|\\.)*>\s*/gm.test(template.parts[0].text),
+          minifyOptions: {
+            minifyCSS: true,
+            minifyJS: true,
+            collapseWhitespace: true,
+            collapseBooleanAttributes: true,
+            collapseInlineTagWhitespace: true,
+            removeOptionalTags: true,
+            removeTagWhitespace: true,
+            sortAttributes: true,
+            sortClassName: true,
+            removeRedundantAttributes: true,
+          }
+        }
+      }),
+      terser(),
+    ] : [],
+  ],
+  // Make all dependencies external
+  external: () => true,
+});
+
+export default inputs.reduce((configs, input) =>
+  [...configs, getConfig(input, false), ...prod ? [getConfig(input, true)] : []], []);
